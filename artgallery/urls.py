9c1fbe86fde0ea66b93bcb6b278c42165a944154
Atul kumar Tiwari <tiwariatul2612@gmail.com>
2021-03-29 00:05:02 +0530

    deployment settings done

--- artgallery/urls.py
+++ artgallery/urls.py
@@ -17,7 +17,12 @@ from django.contrib import admin
 from django.urls import path, include
 from anoop import views
 
+from django.views.static import serve
+from django.conf.urls.static import static
+
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('anoop.urls')),
+    url(r'^media/(?P<path>.*)$', serve,{'document_root':       settings.MEDIA_ROOT}), 
+    url(r'^static/(?P<path>.*)$', serve,{'document_root': settings.STATIC_ROOT}), 
 ]
