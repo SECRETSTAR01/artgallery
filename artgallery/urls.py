1ce23e10bf138779ed1b2c8833a545a2c337adb8
Atul kumar Tiwari <tiwariatul2612@gmail.com>
2021-03-29 00:24:36 +0530

    error resolved

--- artgallery/urls.py
+++ artgallery/urls.py
@@ -16,7 +16,8 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 from anoop import views
-
+from django.conf.urls import url
+from django.conf import settings
 from django.views.static import serve
 from django.conf.urls.static import static
 
