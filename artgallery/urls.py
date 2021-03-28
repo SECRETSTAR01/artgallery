57a1814954fe855c3bfb4f6f6d3c35614959f40a
Atul kumar Tiwari <tiwariatul2612@gmail.com>
2021-03-29 00:36:56 +0530

    --

--- artgallery/urls.py
+++ artgallery/urls.py
@@ -23,7 +23,7 @@ from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('anoop.urls')),
+    path('home', include('anoop.urls')),
     url(r'^media/(?P<path>.*)$', serve,{'document_root':       settings.MEDIA_ROOT}), 
     url(r'^static/(?P<path>.*)$', serve,{'document_root': settings.STATIC_ROOT}),
 ]
